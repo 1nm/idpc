d86d65c5002a89de9409cc0696d616846474fa09
1nm <1nm@users.noreply.github.com>
2016-08-16 02:48:50 +0900

    minor change

--- idpc.py
+++ idpc.py
@@ -10,7 +10,7 @@ class IDPhotoCreator:
         self.dpi = dpi
         self.cw, self.ch = self._mm2px(canvas_size)  # canvas width and height
         self.pw, self.ph = self._mm2px(photo_size)  # photo width and height
-        bw, bh = self._mm2px((10, 10))  # 5mm blank for each edge
+        bw, bh = self._mm2px((10, 10))  # edge blank width and height
         self.cols, self.rows = int((self.cw - bw) / self.pw), int((self.ch - bh)/ self.ph)
 
     def _mm2px(self, size):
@@ -19,31 +19,28 @@ class IDPhotoCreator:
 
     def create(self, photo, output):
         w, h = photo.size
-        if w >= self.pw and h >= self.ph:
-            with photo.clone() as photo_resized:
-                aspect = float(self.pw) / float(self.ph)
-                if float(w) / float(h) > aspect:
-                    w = int(h * aspect)
-                    h = h
-                else:
-                    w = w
-                    h = int(w / aspect)
-                photo_resized.crop(width=w, height=h, gravity='center')
-                photo_resized.resize(self.pw, self.ph)
+        if w < self.pw or h < self.ph:
+            raise TypeError('Image size must be no smaller than {}x{} while size of input is {}x{}'.format(self.pw, self.ph, w, h))
+        with photo.clone() as photo_resized:
+            aspect = float(self.pw) / float(self.ph)
+            if float(w) / float(h) > aspect:
+                w = int(h * aspect)
+            else:
+                h = int(w / aspect)
+            photo_resized.crop(width=w, height=h, gravity='center')
+            photo_resized.resize(self.pw, self.ph)
 
-                with Color('white') as bg:
-                    with Image(width=self.cw, height=self.ch, background=bg) as canvas:
-                        canvas.format = 'jpeg'
-                        l = int((self.cw - self.pw * self.cols) / 2)
-                        t = int((self.ch - self.ph * self.rows) / 2)
-                        for i in range(self.cols):
-                            for j in range(self.rows):
-                                left = l + i * self.pw
-                                top = t + j * self.ph
-                                canvas.composite(top=top, left=left, image=photo_resized)
-                        canvas.save(filename=output)
-        else:
-            print('Image size must be no smaller than {}x{} while size of input is {}x{}'.format(self.pw, self.ph, w, h))
+            with Color('white') as bg:
+                with Image(width=self.cw, height=self.ch, background=bg) as canvas:
+                    canvas.format = 'jpeg'
+                    l = int((self.cw - self.pw * self.cols) / 2)
+                    t = int((self.ch - self.ph * self.rows) / 2)
+                    for i in range(self.cols):
+                        for j in range(self.rows):
+                            left = l + i * self.pw
+                            top = t + j * self.ph
+                            canvas.composite(top=top, left=left, image=photo_resized)
+                    canvas.save(filename=output)
 
 def main():
     parser = argparse.ArgumentParser()
