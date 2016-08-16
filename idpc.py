c8ed7bec803b978390dda8f55daf11275c777128
1nm <1nm@users.noreply.github.com>
2016-08-16 12:11:08 +0900

    fix guide width

--- idpc.py
+++ idpc.py
@@ -18,18 +18,18 @@ class IDPhotoCreator:
         return int(w / 25.4 * self.dpi), int(h / 25.4 * self.dpi)
 
     def _draw_guide(self, canvas):
-        if self.guide:
+        if self.guide > 0:
             with Drawing() as draw:
-                draw.stroke_color = Color('silver')
-                draw.fill_color = Color('white')
-                draw.stroke_width = 1
-                draw.stroke_dash_array = 8,9
-                l = int((self.cw - self.pw * self.cols - (self.cols + 1)) / 2)
-                t = int((self.ch - self.ph * self.rows - (self.rows + 1)) / 2)
+                draw.stroke_color = Color('black')
+#                draw.fill_color = Color('white')
+                draw.stroke_width = self.guide
+#                draw.stroke_dash_array = 8,9
+                l = int((self.cw - self.pw * self.cols - (self.cols + 1) * self.guide) / 2)
+                t = int((self.ch - self.ph * self.rows - (self.rows + 1) * self.guide) / 2)
                 for i in range(self.rows + 1):
-                    draw.line((l, t + i * (self.ph + 1)), (l + self.pw * self.cols + self.cols, t + i * (self.ph + 1)))
+                    draw.line((l - int(self.guide / 2), t + i * (self.ph + self.guide)), (l + int(self.guide / 2) + (self.pw + self.guide) * self.cols, t + i * (self.ph + self.guide)))
                 for j in range(self.cols + 1):
-                    draw.line((l + j * (self.pw + 1), t), (l + j * (self.pw + 1), t + self.ph * self.rows + self.rows))
+                    draw.line((l + j * (self.pw + self.guide), t - int(self.guide / 2)), (l + j * (self.pw + self.guide), t + int(self.guide / 2) + (self.ph + self.guide) * self.rows))
                 draw(canvas)
 
     def create(self, photo, output):
@@ -49,12 +49,12 @@ class IDPhotoCreator:
             with Color('white') as bg:
                 with Image(width=self.cw, height=self.ch, background=bg, resolution=self.dpi) as canvas:
                     self._draw_guide(canvas)
-                    l = int((self.cw - self.pw * self.cols - (self.cols + 1 if self.guide else 0)) / 2)
-                    t = int((self.ch - self.ph * self.rows - (self.rows + 1 if self.guide else 0)) / 2)
+                    l = int((self.cw - self.pw * self.cols - (self.cols + 1) * self.guide) / 2)
+                    t = int((self.ch - self.ph * self.rows - (self.rows + 1) * self.guide) / 2)
                     for i in range(self.cols):
                         for j in range(self.rows):
-                            left = l + i * self.pw + (i + 1 if self.guide else 0)
-                            top = t + j * self.ph + (j + 1 if self.guide else 0)
+                            left = l + i * self.pw + (i + 1) * self.guide - int(self.guide / 2)
+                            top = t + j * self.ph + (j + 1) * self.guide - int(self.guide / 2)
                             canvas.composite(top=top, left=left, image=photo_resized)
                     canvas.save(filename=output)
 
@@ -65,11 +65,11 @@ def main():
     parser.add_argument('--photo-size', nargs='?', default='35x45', help='photo size (size of each tile) widthxheight in mm, default value is 35x45')
     parser.add_argument('--print-size', nargs='?', default='89x127', help='print size (size of output image) widthxheight in mm, default value is 89x127 (L size)')
     parser.add_argument('--dpi', nargs='?', type=int, default='600', help='dpi, default value is 600')
-    parser.add_argument('--guide', action='store_true', help='guide will be drawn if True')
+    parser.add_argument('--guide-width', nargs='?', type=int, default=0, help='guide width, default value is 0 (no guide)')
     args = parser.parse_args()
     photo_size = map(int, args.photo_size.split('x'))
     canvas_size = map(int, args.print_size.split('x'))
-    idpc = IDPhotoCreator(photo_size, canvas_size, args.dpi, args.guide)
+    idpc = IDPhotoCreator(photo_size, canvas_size, args.dpi, args.guide_width)
     with Image(filename=args.input) as photo:
         idpc.create(photo, args.output)
 
